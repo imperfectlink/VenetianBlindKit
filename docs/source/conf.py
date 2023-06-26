e03276c37cf1bcd7bf4d34950160d6af961fa1cd
ImperfectLink <imperfectlink@gmail.com>
2023-06-26 10:14:49 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Venetian Blind Kit'
+copyright = '2023, ImperfectLink'
+author = 'ImperfectLink'
 
 release = '0.1'
 version = '0.1.0'
